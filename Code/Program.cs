136255494ae62389b877e59aeef01340b3a2b6e8
Maksim <you@epic.twod@mail.ru>
2022-07-29 22:55:26 +0300

    First part of program is ready

--- Code/Program.cs
+++ Code/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿string[] newStrArray(int arrayLength)
+{
+    string[] array = new string[arrayLength];
+    for(int i = 0; i < arrayLength; i++)
+    {
+        Console.Write($"Введите {i+1} элемент массива: ");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+Console.Write("Введите длинну начального массива: ");
+int arrayLength = Convert.ToInt32(Console.ReadLine());
+
+string[] textArray = newStrArray(arrayLength);
+
